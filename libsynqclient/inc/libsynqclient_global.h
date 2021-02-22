c48e7fd8bb4dc2a33df33d6b1a33e7997c9fd315
Martin Hoeher <martin@rpdev.net>
2021-02-22 23:30:28 +0100

    Allow building/linking static on Windows

--- libsynqclient/inc/libsynqclient_global.h
+++ libsynqclient/inc/libsynqclient_global.h
@@ -3,10 +3,14 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(LIBSYNQCLIENT_LIBRARY)
-#  define LIBSYNQCLIENT_EXPORT Q_DECL_EXPORT
+#if defined(LIBSYNQCLIENT_LIBRARY_STATIC)
+#    define LIBSYNQCLIENT_EXPORT
 #else
-#  define LIBSYNQCLIENT_EXPORT Q_DECL_IMPORT
+#    if defined(LIBSYNQCLIENT_LIBRARY)
+#        define LIBSYNQCLIENT_EXPORT Q_DECL_EXPORT
+#    else
+#        define LIBSYNQCLIENT_EXPORT Q_DECL_IMPORT
+#    endif
 #endif
 
 #endif // LIBSYNQCLIENT_GLOBAL_H
